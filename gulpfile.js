d560362b36757d4cdaa6e709daf906252dde0bab
davidtamaki <davidszajngarten@gmail.com>
2022-02-12 16:46:21 +0000

    updating basic info

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var replace = require('gulp-replace');
+var dotenv = require('dotenv').config()
 
 // compile scss to css
 gulp.task('sass', function () {
@@ -18,13 +20,16 @@ gulp.task('sass:watch', function () {
     gulp.watch('./sass/**/*.scss', ['sass']);
 });
 
-// minify js
-gulp.task('minify-js', function () {
+// minify js and set env variables 
+gulp.task('build-js', function () {
     return gulp.src('./js/scripts.js')
+        .pipe(replace('${INVITE_CODE}', process.env.INVITE_CODE))
+        .pipe(replace('${GOOGLE_APPS_SCRIPT_POST_URL}', process.env.GOOGLE_APPS_SCRIPT_POST_URL))
         .pipe(uglify())
         .pipe(rename({basename: 'scripts.min'}))
         .pipe(gulp.dest('./js'));
 });
 
 // default task
-gulp.task('default', gulp.series('sass', 'minify-js'));
+gulp.task('default', gulp.series('sass', 'build-js'));
+
--- js/scripts.js
+++ js/scripts.js
@@ -137,6 +137,7 @@ $(document).ready(function () {
     });
 
     /********************** Social Share buttons ***********************/
+    /*
     var share_bar = document.getElementsByClassName('share-bar');
     var po = document.createElement('script');
     po.type = 'text/javascript';
@@ -160,6 +161,7 @@ $(document).ready(function () {
         share_bar[i].innerHTML = html;
         share_bar[i].style.display = 'inline-block';
     }
+    */
 
     /********************** Embed youtube video *********************/
     $('.player').YTPlayer();
@@ -184,23 +186,23 @@ $(document).ready(function () {
         },
         data: {
             // Event title
-            title: "Ram and Antara's Wedding",
+            title: "David and Crystal's Wedding",
 
             // Event start date
-            start: new Date('Nov 27, 2017 10:00'),
+            start: new Date('Oct 14, 2022 17:00'),
 
             // Event duration (IN MINUTES)
             // duration: 120,
 
             // You can also choose to set an end time
             // If an end time is set, this will take precedence over duration
-            end: new Date('Nov 29, 2017 00:00'),
+            end: new Date('Oct 15, 2022 00:00'),
 
             // Event Address
-            address: 'ITC Fortune Park Hotel, Kolkata',
+            address: 'Casa Rural La Granja de Bastian, Tenerife',
 
             // Event Description
-            description: "We can't wait to see you on our big day. For any queries or issues, please contact Mr. Amit Roy at +91 9876543210."
+            description: "We can't wait to see you on our big day. For any queries or issues, please contact us at david4crystal@gmail.com"
         }
     });
 
@@ -214,11 +216,10 @@ $(document).ready(function () {
 
         $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
 
-        if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
-            && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
+        if (MD5($('#invite_code').val()) !== '${INVITE_CODE}') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbzUqz44wOat0DiGjRV1gUnRf4HRqlRARWggjvHKWvqniP7eVDG-/exec', data)
+            $.post('${GOOGLE_APPS_SCRIPT_POST_URL}', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
@@ -241,9 +242,9 @@ $(document).ready(function () {
 
 // Google map
 function initMap() {
-    var location = {lat: 22.5932759, lng: 88.27027720000001};
+    var location = {lat: 28.135037, lng: -16.521024};
     var map = new google.maps.Map(document.getElementById('map-canvas'), {
-        zoom: 15,
+        zoom: 13,
         center: location,
         scrollwheel: false
     });
@@ -254,19 +255,6 @@ function initMap() {
     });
 }
 
-function initBBSRMap() {
-    var la_fiesta = {lat: 20.305826, lng: 85.85480189999998};
-    var map = new google.maps.Map(document.getElementById('map-canvas'), {
-        zoom: 15,
-        center: la_fiesta,
-        scrollwheel: false
-    });
-
-    var marker = new google.maps.Marker({
-        position: la_fiesta,
-        map: map
-    });
-}
 
 // alert_markup
 function alert_markup(alert_type, msg) {
